734d47c02591d36a37a8d58d4ae26855ca8b3452
David Patiño <david.andrespr5@gmail.com>
2022-04-23 01:09:37 -0500

    16. Agregando funcion metodo getInfo en class UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,7 +1,10 @@
 const User = require('./../models/User')
 class UserService{
     static create(id,username,name){
-        return new User(id,username,name,'bio')
+        return new User(id,username,name,'Sin bio')
+    }
+    static getInfo(obj){
+        return [obj.id,obj.username,obj.name,'Sin bio']
     }
 }
 
