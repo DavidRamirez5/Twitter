dd6c19301491fd1a03d2ce7ade6b6e65d5abd2db
David Patiño <david.andrespr5@gmail.com>
2022-04-22 19:36:46 -0500

    7. Modificando Test para cumplir Requerimiento 2

--- test/User.test.js
+++ test/User.test.js
@@ -2,12 +2,12 @@ const User = require('./../app/models/User')
 describe("Unit Test for User class", () =>{
     test("1) Create an User object",() => {
         // Codigo de app
-        const user = new User(1,'davidramirez5','David','bio','dateCreated','lastUpdated')
+        const user = new User(1,'davidramirez5','David','bio')
         expect(user.id).toBe(1)
         expect(user.username).toBe("davidramirez5")
         expect(user.name).toBe("David")
         expect(user.bio).toBe("bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined() // Verifica que el valor no sea undefined
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
