5b122b211cb752c72c0f75e27c912bdcef57d4bb
David Patiño <david.andrespr5@gmail.com>
2022-04-23 11:33:59 -0500

    Agregando uddateUserUsername() en class UserService

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,8 +4,12 @@ class UserService{
         return new User(id,username,name,'Sin bio')
     }
     static getInfo(obj){
-        return [obj.id,obj.username,obj.name,'Sin bio']
+        return [obj.id,obj.username,obj.name,'Sin bio'] // Objetct.values()
     }
+    static updateUserUsername(user,newusername){
+        return user.username = newusername
+    }
+
 }
 
 module.exports= UserService
