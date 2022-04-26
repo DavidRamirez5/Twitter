086bafe15ebb5bff53f296e5ee507b5e57591fce
David Patiño <david.andrespr5@gmail.com>
2022-04-25 19:39:07 -0500

    22. Creando clase UserView y metodo createUser

---
+++ app/views/UserView.js
@@ -0,0 +1,11 @@
+class UserView{
+    static createUser(payload){
+
+        if (payload === null) {return payload={error:'payload no existe'}}
+    }
+}
+
+
+//console.log(UserView.createUser(null))
+
+module.exports= UserView
