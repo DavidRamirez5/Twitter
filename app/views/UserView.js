71589dd2031b0b66248e86176718446d30d6feb8
David Patiño <david.andrespr5@gmail.com>
2022-04-28 15:38:08 -0500

    24. Agregando condicional para verificar entrada de propiedades

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,7 +1,11 @@
 class UserView{
     static createUser(payload){
 
-        if (payload === null) {return payload={error:'payload no existe'}}
+        if (payload === null) {
+            return payload={error:'payload no existe'}
+        }else if(typeof payload.username != 'string' && typeof payload.name != 'string' && typeof payload.id != 'number'){
+            return  payload={rojo:'necesitan tener un valor válido'}
+        }
     }
 }
 
